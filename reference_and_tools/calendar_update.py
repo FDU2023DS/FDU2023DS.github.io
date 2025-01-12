82b961fb8336a1ef48eb3f4da95fc36095ea66d2
foxabbage <wolfhenryyh@gmail.com>
2025-01-12 15:05:55 +0800

    1.0

--- reference_and_tools/calendar_update.py
+++ reference_and_tools/calendar_update.py
@@ -3,14 +3,14 @@ from zoneinfo import ZoneInfo
 import re
 
 date=datetime.datetime.now(datetime.timezone.utc).replace(microsecond=0).isoformat()
-date = str(date)+'+08:00'
+date = date.replace('+00:00',"+08:00',")
 
 with open('_pages/calendar.md', 'r+', encoding='utf-8') as fp:
     lines=fp.readlines()
     flag=0
     for line in lines:
         if re.search(r'initialDate:', line):
-            line = re.sub(r'....-..-..T..:..:.....:..', date, line)
+            line = re.sub(r'....-..-..T..:..:.....:..*', date, line)
             lines[flag] = line
             break
         flag+=1
